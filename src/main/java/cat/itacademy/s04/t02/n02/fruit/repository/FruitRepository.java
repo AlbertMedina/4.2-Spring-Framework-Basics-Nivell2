58293df9b76619d6104c21af6b70431f60a19e93
Albert Medina <albertmecu@gmail.com>
2025-12-15 13:19:28 +0100

    feat: added repository

--- src/main/java/cat/itacademy/s04/t02/n02/fruit/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/repository/FruitRepository.java
@@ -4,4 +4,6 @@ import cat.itacademy.s04.t02.n02.fruit.model.Fruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
+
+    boolean existsByProviderId(Long providerId);
 }
