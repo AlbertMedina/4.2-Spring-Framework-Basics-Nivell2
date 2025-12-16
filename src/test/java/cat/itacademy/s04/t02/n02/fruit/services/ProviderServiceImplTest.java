200cd68ab366f8e302858289ec25867c65b7bdc3
Albert Medina <albertmecu@gmail.com>
2025-12-16 13:51:08 +0100

    test: added tests for provider service

---
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/services/ProviderServiceImplTest.java
@@ -0,0 +1,111 @@
+package cat.itacademy.s04.t02.n02.fruit.services;
+
+import cat.itacademy.s04.t02.n02.fruit.dto.ProviderDTO;
+import cat.itacademy.s04.t02.n02.fruit.model.Provider;
+import cat.itacademy.s04.t02.n02.fruit.repository.FruitRepository;
+import cat.itacademy.s04.t02.n02.fruit.repository.ProviderRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ProviderServiceImplTest {
+
+    @Mock
+    private FruitRepository fruitRepository;
+
+    @Mock
+    private ProviderRepository providerRepository;
+
+    @InjectMocks
+    private ProviderServiceImpl providerService;
+
+    @Test
+    void createProvider_shouldSaveProvider() {
+        ProviderDTO providerDTO = new ProviderDTO("Albert", "Spain");
+
+        when(providerRepository.save(any(Provider.class))).thenAnswer(i -> i.getArgument(0));
+
+        Provider provider = providerService.createProvider(providerDTO);
+
+        assertNotNull(provider);
+        assertEquals("Albert", provider.getName());
+        assertEquals("Spain", provider.getCountry());
+
+        verify(providerRepository).save(any(Provider.class));
+    }
+
+    @Test
+    void updateProvider_shouldUpdateExistingProvider() {
+        Provider provider = new Provider("Albert", "Spain");
+
+        when(providerRepository.findById(1L)).thenReturn(Optional.of(provider));
+        when(providerRepository.save(any(Provider.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        ProviderDTO providerDTO = new ProviderDTO("Albert", "Spain");
+
+        Provider updated = providerService.updateProvider(1L, providerDTO);
+
+        assertEquals("Albert", updated.getName());
+        assertEquals("Spain", updated.getCountry());
+
+        verify(providerRepository).findById(1L);
+        verify(providerRepository).save(any(Provider.class));
+    }
+
+    @Test
+    void removeProvider_shouldDeleteExistingProvider() {
+        Long providerId = 1L;
+
+        when(providerRepository.existsById(providerId)).thenReturn(true);
+        doNothing().when(providerRepository).deleteById(providerId);
+
+        assertDoesNotThrow(() -> providerService.removeProvider(providerId));
+
+        verify(providerRepository).existsById(providerId);
+        verify(providerRepository).deleteById(providerId);
+    }
+
+    @Test
+    void getProviderById_shouldReturnProvider() {
+        Provider provider = new Provider("Albert", "Spain");
+
+        when(providerRepository.findById(1L)).thenReturn(Optional.of(provider));
+
+        Provider result = providerService.getProviderById(1L);
+
+        assertNotNull(result);
+        assertEquals("Albert", result.getName());
+        assertEquals("Spain", result.getCountry());
+
+        verify(providerRepository).findById(1L);
+    }
+
+    @Test
+    void getAllProvider_shouldReturnListOfProvider() {
+        Provider provider1 = new Provider("Albert", "Spain");
+        Provider provider2 = new Provider("Joao", "Portugal");
+
+        when(providerRepository.findAll()).thenReturn(Arrays.asList(provider1, provider2));
+
+        List<Provider> providers = providerService.getAllProviders();
+
+        assertEquals(2, providers.size());
+        assertEquals("Albert", providers.get(0).getName());
+        assertEquals("Joao", providers.get(1).getName());
+
+        verify(providerRepository).findAll();
+    }
+}
